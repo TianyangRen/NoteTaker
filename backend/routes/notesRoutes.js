25c0ae47f9530501b1ae5ee0b19c7af106ada295
Tianyang Ren <victorren86@gmail.com>
2025-12-21 11:07:32 -0500

    Update notesRoutes.js

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).send('Hello World!123');
+  res.status(200).send('You just fetched all notes');
 });
 
 router.post('/', (req, res) => {
